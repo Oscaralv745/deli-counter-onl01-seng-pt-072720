e194a59c16ac2f5bc2b0a63867b9049aaed7a1e3
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-02 19:48:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(array)
   if array == 0 
     puts "The line is currently empty."
   else
-    array.join("The line is currently:")
+    array.join("The line is currently: ")
+    
   end
 end
