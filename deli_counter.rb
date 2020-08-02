8434549b604f31aece764fb80a9850abb98e3512
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-02 19:46:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,8 @@
 # Write your code here.
 def line(array)
   if array == 0 
-    puts "the line is currently empty."
+    puts "The line is currently empty."
   else
-    
-    array.each_with_index(1) do |i, x|
-      puts "The line is currently: #{i}. #{x} "
-    end
+    array.join
   end
 end
