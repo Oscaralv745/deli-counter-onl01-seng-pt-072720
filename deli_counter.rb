58faced4a9a44de1c71b1c802b9d27d781cc418a
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-02 19:46:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(array)
   if array == 0 
     puts "the line is currently empty."
   else
+    
     array.each_with_index(1) do |i, x|
       puts "The line is currently: #{i}. #{x} "
     end
